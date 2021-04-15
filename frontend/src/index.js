c56ab847b5cd051ae92a9be4f773c0c8111b4c83
viviruska <vivien.ruska@prezi.com>
2021-04-15 22:09:14 +0200

    feat: Adds thunk middleware for async action creators and also logger middleware for debugging and state changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import middleware from './middleware'
+// import reportWebVitals from './reportWebVitals';
+
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
