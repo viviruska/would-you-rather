a22b42a470a2580317f917e0c97d7ba8f92412fd
viviruska <vivien.ruska@prezi.com>
2021-04-17 22:28:23 +0200

    feat: Builds out UI based on mockups for Poll Component

--- frontend/src/components/Poll.js
+++ frontend/src/components/Poll.js
@@ -6,14 +6,31 @@ class Poll extends Component {
     console.log(this.props)
     const { id, poll, author } = this.props;
 
+    if (poll === null) {
+      return <p>This poll doesn't exist</p>
+    }
+
+    const { optionOne, optionTwo } = poll
+    const { name, avatarURL } = author
+
     return (
       <div className='poll-item'>
         <div className='poll-header'>
-          <span className='header-text'>{author.name} asks:</span>
+          <span className='poll-header-text'>{name} asks:</span>
         </div>
-        <div>{author.avatarURL}</div>
-        <div>View Poll button</div>
-        {id}
+        <div className='poll-overview'>
+          <img 
+            src={avatarURL}
+            alt={`Avatar of ${name}`}
+            className='avatar'
+          />
+          <div className='poll-info'>
+            <div className='divider'></div>
+            <span className='poll-text general'>Would you rather</span>
+            <span className='poll-text'>{ `...${optionOne.text}...` }</span>
+            <button className='view-poll-btn'>View Poll</button>
+          </div>
+        </div>
       </div>
     )
   }
@@ -21,7 +38,7 @@ class Poll extends Component {
 
 function mapStateToProps({ polls, users }, { id }) {
   const poll = polls[id]
-  const author = users[poll.author]
+  const author = poll ? users[poll.author] : null
 
   return {
     poll: poll,  // formatPoll?
